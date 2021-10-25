ddfacfa5c926f73c4325029ab4b4a79314c4b197
Alex Riley <88731829+AlRiley90@users.noreply.github.com>
2021-10-25 16:26:54 -0500

    Delete keys.js

--- keys.js
+++
@@ -1,6 +0,0 @@
-var test = "test string";
-var mapBoxKey = "pk.eyJ1IjoiYWxyaWxleTEyIiwiYSI6ImNrdWN6eGE0NTE1aWIyb256MnU4ejBwa3YifQ.FL4TJrT8YzSfB19EtskCnw"
-
-var openWeatherKey = "ab57dced75a708e1a783c694211b823b"
-
-var gitHubKey = "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"
